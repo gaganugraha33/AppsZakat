ef4874a4948b9481545f269ece9cf51e0238850a
gaganugraha33 <gaganugraha33@gmail.com>
2017-02-21 07:35:18 +0700

    fix splash screen, fix main menu layout

--- app/src/main/java/inc/angga/mobile/appszakat/MainActivity.java
+++ app/src/main/java/inc/angga/mobile/appszakat/MainActivity.java
@@ -3,23 +3,47 @@ package inc.angga.mobile.appszakat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btToHome;
+    private ImageView btToHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btToHome = (Button)findViewById(R.id.bt_to_home);
+        btToHome = (ImageView)findViewById(R.id.bt_to_home);
 
-        btToHome.setOnClickListener(new View.OnClickListener() {
+//        btToHome.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        btToHome.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                        startActivity(intent);
+
+                        btToHome.setImageDrawable(getResources().getDrawable(R.drawable.icon_home_clicked));
+
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        btToHome.setImageDrawable(getResources().getDrawable(R.drawable.icon_home));
+                        break;
+                    default:
+                        break;
+                }
+
+                return true;
             }
         });
     }
--- app/src/main/java/inc/angga/mobile/appszakat/SplashScreen.java
+++ app/src/main/java/inc/angga/mobile/appszakat/SplashScreen.java
@@ -1,20 +1,26 @@
 package inc.angga.mobile.appszakat;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by ACER on 2/1/2017.
  */
 
 public class SplashScreen extends AppCompatActivity {
+    private View rlProgresBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen_layout);
+        rlProgresBar = findViewById(R.id.rl_progress_bar);
         startDelay();
     }
 
@@ -22,10 +28,12 @@ public class SplashScreen extends AppCompatActivity {
         Thread timerThread = new Thread() {
             public void run() {
                 try {
+                    showProgress(true);
                     sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
+                    showProgress(false);
                     Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                     startActivity(intent);
                     finish();
@@ -33,5 +41,28 @@ public class SplashScreen extends AppCompatActivity {
             }
         };
         timerThread.start();
+    }
+
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = 200;
+
+//            rlProgresBar.setVisibility(View.VISIBLE);
+            rlProgresBar.animate().setDuration(shortAnimTime)
+                    .alpha(show ? 1 : 0)
+                    .setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            rlProgresBar.setVisibility(show ? View.VISIBLE
+                                    : View.GONE);
+                        }
+                    });
+        } else {
+
+            rlProgresBar.setVisibility(show ? View.VISIBLE : View.GONE);
+        }
     }
 }
