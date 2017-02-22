ea909be8fa9fae9bd794abae690f04d92448dbab
gaganugraha33 <gaganugraha33@gmail.com>
2017-02-22 07:40:42 +0700

    add startmenu fragment, add home fragment, delete home activity

--- app/src/main/java/inc/angga/mobile/appszakat/MainActivity.java
+++ app/src/main/java/inc/angga/mobile/appszakat/MainActivity.java
@@ -6,46 +6,19 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView btToHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btToHome = (ImageView)findViewById(R.id.bt_to_home);
 
-//        btToHome.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fl_container, StartMenuFragment.newInstance(MainActivity.this, getSupportFragmentManager())).commit();
 
-        btToHome.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                switch (motionEvent.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                        startActivity(intent);
-
-                        btToHome.setImageDrawable(getResources().getDrawable(R.drawable.icon_home_clicked));
-
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        btToHome.setImageDrawable(getResources().getDrawable(R.drawable.icon_home));
-                        break;
-                    default:
-                        break;
-                }
-
-                return true;
-            }
-        });
     }
 
 }
--- app/src/main/java/inc/angga/mobile/appszakat/menu_definisi_zakat/MenuZakatActivity.java
+++ app/src/main/java/inc/angga/mobile/appszakat/menu_definisi_zakat/MenuZakatActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
-import inc.angga.mobile.appszakat.HomeActivity;
 import inc.angga.mobile.appszakat.R;
 
 /**
